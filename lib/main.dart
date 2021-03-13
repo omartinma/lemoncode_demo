96fca02b05a6b5bf04221843fd755d2f0100b2f4
Oscar Martín Martín <martinm.oscar@gmail.com>
2021-03-13 10:54:38 +0100

    List page and detail

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:lemoncode_demo/presentation/ui/characters_screen.dart';
+import 'package:lemoncode_demo/core/services/api.dart';
+import 'package:lemoncode_demo/presentation/controllers/characters_controller.dart';
+import 'package:lemoncode_demo/presentation/ui/characters_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,10 +12,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(),
-      home: CharactersScreen(),
+    return MultiProvider(
+      providers: [
+        Provider(
+          create: (context) => CharactersApi(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => CharactersController(
+              charactersApi: context.read<CharactersApi>()),
+        ),
+      ],
+      builder: (context, child) => MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(),
+        home: CharactersPage(),
+      ),
     );
   }
 }
